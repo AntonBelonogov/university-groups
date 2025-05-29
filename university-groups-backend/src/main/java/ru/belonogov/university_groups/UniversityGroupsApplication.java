e705cdea6c4c330553c240c3b01dbec14ca152ce
belonogov <belonogov@isands.ru>
2025-05-29 21:55:19 +0700

    Refactor directories path + docker-compose.yaml fixes

--- university-groups-backend/src/main/java/ru/belonogov/university_groups/UniversityGroupsApplication.java
+++ university-groups-backend/src/main/java/ru/belonogov/university_groups/UniversityGroupsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UniversityGroupsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UniversityGroupsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityGroupsApplication.class, args);
+    }
 
 }
